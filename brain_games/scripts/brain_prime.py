5afa917e11c1d4fa389ad5000cfdf3179faf8e22
Andrey <andrei_chesanov@mail.ru>
2024-09-22 10:46:37 +0300

    brain_prime change

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -19,11 +19,10 @@ def simple_val_or_not(name):
 
         if value == (2 or 3 or 5 or 7):
             simple_or_not = 'no'
-
-        if value > 3 and (value % 2 != 0
-                          and value % 3 != 0
-                          and value % 5 != 0
-                          and value % 7 != 0):
+        elif value > 3 and (value % 2 != 0
+                            and value % 3 != 0
+                            and value % 5 != 0
+                            and value % 7 != 0):
             simple_or_not = 'yes'
         else:
             simple_or_not = 'no'
