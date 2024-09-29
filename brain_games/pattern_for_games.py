aa7c7a87fe0a6fdf97a6d0f6d235616700d33315
Andrey <andrei_chesanov@mail.ru>
2024-09-29 16:41:35 +0300

    new pattern

--- brain_games/pattern_for_games.py
+++ brain_games/pattern_for_games.py
@@ -1,15 +1,14 @@
 # Паттерн для скриптов
 def pattern(func, name):
-    counter = 0
-    while counter < 3:
+    for i in range(3):
         a = func()
         if a[0] == a[1]:
             print("Correct!")
-            counter += 1
         else:
             print(f"'{a[0]}' is wrong answer ;(. "
                   f"Correct answer was '{a[1]}'.")
             print(f"Let's try again, {name}!")
             break
-    if counter == 3:
+    else:
         print(f"Congratulations, {name}!")
+
