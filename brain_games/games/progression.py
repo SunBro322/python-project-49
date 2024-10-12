3cbee6fc8ece345ec47af80a886bdae09ec2d8a3
Andrey <andrei_chesanov@mail.ru>
2024-10-12 21:48:21 +0300

    refactoring brain-progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,28 +4,14 @@ from brain_games import constant
 
 def generate_round_data():
     generate_round_data.DESCRIPTION = constant.DESCRIPTION_PROGRESSION
-    array_of_values = []
-    for i in range(1, 100):
-        array_of_values.append(i)
-
-    value_for_slice = 0
-    invisible_value = 0
-
-    step_value = random.randint(1, 5)
-    if 1 <= step_value <= 2:
-        value_for_slice = 10
-    else:
-        value_for_slice = 30
-
-    start_value = random.randint(0, 20)
-    finish_value = start_value + value_for_slice
-
-    slice_of_values = array_of_values[start_value:finish_value:step_value]
-    invisible_value = slice_of_values[random.randint(0,
-                                                     len(slice_of_values) - 1)]
-    slice_of_values[slice_of_values.index(invisible_value)] = '..'
-    result = ' '.join(map(str, slice_of_values))
-
+    start = 1
+    end = 100
+    step = 4
+    progression = list(range(start, end + 1, step))
+    random_index = random.randint(start, len(progression))
+    invisible_value = progression[random_index]
+    progression[random_index] = ".."
+    result = ' '.join(map(str, progression))
     question_for_gamers = result
 
     return question_for_gamers, str(invisible_value)
